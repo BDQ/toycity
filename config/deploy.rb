89594ebf4a6066e7a6e10b29ad7807da87a431f2
Brian Quinn <briandquinn@gmail.com>
2008-11-20 10:03:20 +0000

    Updated deploy and database configs

---
+++ config/deploy.rb
@@ -0,0 +1,20 @@
+set :application, "toycity.ie"
+set :repository,  "git@github.com:BDQ/toycity.git"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/data/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+# set :scm, :subversion
+set :scm, "git"
+default_run_options[:pty] = true
+set :scm_passphrase, ""  
+set :user, "deploy"
+
+
+role :app, "10.1.0.29"
+role :web, "10.1.0.29"
+role :db,  "10.1.0.29", :primary => true
